6b1da92439c33d9ac4ca08939ff3959fa3b52af5
diegotalejandro <diego.torreblanca2013@gmail.com>
2020-08-27 17:51:45 -0400

    Some little changes

---
+++ build/precache-manifest.6a3064fac79f11cd9d57ae1e3f867a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ea5c1eb0a2a84273a5324852eeac2a",
+    "url": "/react-firebase-crud-app/index.html"
+  },
+  {
+    "revision": "5b91ddaed7a9c3cfd7c6",
+    "url": "/react-firebase-crud-app/static/css/2.0eceeaeb.chunk.css"
+  },
+  {
+    "revision": "bc075f74c0577e3c0479",
+    "url": "/react-firebase-crud-app/static/css/main.cc6652e4.chunk.css"
+  },
+  {
+    "revision": "5b91ddaed7a9c3cfd7c6",
+    "url": "/react-firebase-crud-app/static/js/2.784d4458.chunk.js"
+  },
+  {
+    "revision": "5760e02b44324bff757273b96337eba4",
+    "url": "/react-firebase-crud-app/static/js/2.784d4458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc075f74c0577e3c0479",
+    "url": "/react-firebase-crud-app/static/js/main.99f9d4dd.chunk.js"
+  },
+  {
+    "revision": "3473fa2c903cc57271f2",
+    "url": "/react-firebase-crud-app/static/js/runtime-main.fdf52485.js"
+  }
+]);
